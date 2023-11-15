8ff3f05288e63939c4ab29d54a437cc492015eaa
Rodrigo Farias <iam.rodrigofarias@gmail.com>
2023-11-15 17:30:22 +0100

    delete taskdone endpoint

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -51,30 +51,6 @@ exports.editTask = (req, res) => {
     });
 };
 
-exports.doneTask = (req, res) => {
-    const { is_done } = req.body;
-    const { id } = req.params;
-
-    if (!is_done) {
-        return res.status(400).json({ error: 'All attributes (is_done) are mandatory.' });
-    }
-
-    db.run('UPDATE tbl_task SET is_done = ? WHERE id = ?', [is_done, id], function (err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-
-        const taskId = this.lastID;
-
-        db.get('SELECT * FROM tbl_task WHERE id = ?', [taskId], (err, row) => {
-            if (err) {
-                return res.status(500).json({ error: err.message });
-            }
-            res.status(200).json(row);
-        });
-    });
-};
-
 exports.deleteTask = (req, res) => {
     const { id } = req.params;
 
